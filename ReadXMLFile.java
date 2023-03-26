a99386f7f6e36870f88f2a919883d292d8bac3cc
Valravns <127699226+Valravns@users.noreply.github.com>
2023-03-26 23:55:08 +0300

    Add files via upload

--- ReadXMLFile.java
+++ ReadXMLFile.java
@@ -12,19 +12,19 @@ public class ReadXMLFile {
     public static void main(String[] args) {
         try{
             File getXMLFile = new File("Info.xml");
-            DocumentBuilderFactory dbFact = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dbBuild = dbFact.newDocumentBuilder();
-            Document doc = dbBuild.parse(getXMLFile);
+            DocumentBuilderFactory xmlDBF = DocumentBuilderFactory.newInstance();
+            DocumentBuilder xmlDB = xmlDBF.newDocumentBuilder();
+            Document xmlDocument = xmlDB.parse(getXMLFile);
 
 
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
-            NodeList nList  = doc.getElementsByTagName("person");
+            System.out.println("Root element: " + xmlDocument.getDocumentElement().getNodeName());
+            NodeList nodeList1  = xmlDocument.getElementsByTagName("person");
 
-            for(int i=0; i <nList.getLength();i++) {
-                Node nNode = nList.item(i);
-                System.out.println("Node name " + nNode.getNodeName() + " " + (i+1));
-                if(nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
+            for(int i=0; i <nodeList1.getLength();i++) {
+                Node node = nodeList1.item(i);
+                System.out.println("Node name " + node.getNodeName() + " " + (i+1));
+                if(node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
                     System.out.println("Person ID#: " + eElement.getAttribute("id"));
                     System.out.println("Person Name: " + eElement.getElementsByTagName("name").item(0).getTextContent());
                     System.out.println("Person Address: " + eElement.getElementsByTagName("address").item(0).getTextContent());
